d8b35100598c1b2af909e41d72d3462820b5ddf2
DarkSnake749 <etienne.boule@hotmail.com>
2026-05-24 18:17:38 -0400

    Settings up macroquad

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+#[macroquad::main("Rust Asteroids")]
+async fn main() {
+    loop {
+        clear_background(BLACK);
+        draw_text("Hello, macroquad!", 0.0, 45.0, 70.0, WHITE);
+        next_frame().await;
+    }
 }
