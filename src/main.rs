4332fbdf4344262bb377de2b0bf3f8201e54152f
DarkSnake749 <etienne.boule@hotmail.com>
2026-05-24 18:32:44 -0400

    Written a rot to vec function, ship structure and a bunch of ship constant.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
 use macroquad::prelude::*;
 
+const SHIP_HEIGHT: f32 = 25.;
+const SHIP_BASE: f32 = 22.;
+const SHIP_ACC: f32 = 0.33;
+
+/// Important note: speed rotation in degrees.
+const SHIP_ROT_SPEED: f32 = 5.;
+
+struct Ship {
+    pos: Vec2,
+    rot: f32,
+    vel: Vec2,
+}
+
 #[macroquad::main("Rust Asteroids")]
 async fn main() {
     loop {
@@ -8,3 +21,8 @@ async fn main() {
         next_frame().await;
     }
 }
+
+/// Returns a Vec2 from a rotation in radians.
+fn vec_from_rad(rad: f32) -> Vec2 {
+    return Vec2::new(rad.sin(), -rad.cos());
+} 
